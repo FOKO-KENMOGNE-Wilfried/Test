80c001da2415b4209cb774bf5426171c65690fbb
FOKO KENMOGNE Wilfried <143821047+FOKO-KENMOGNE-Wilfried@users.noreply.github.com>
2024-04-08 15:11:16 +0100

    He reamain the logic to the basket

---
+++ src/components/views/BasketCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{ 
+	pro: { id: number; name: string; price: string; spec: string; image: string; type: boolean; }[];
+}>()
+</script>
+
+<template>
+	<div class="box">
+	    <img class="basket_image" :src="pro[0].image" alt="product">
+	</div>
+</template>
+
+<style scoped>
+
+  .basket_image{
+    width: 60px;
+    height: 60px;
+  }
+  .box{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    color: black;
+    padding: 5px;
+	height: 70px;
+    border-radius: 10px;
+  }
+  
+</style>
--- src/components/views/Products.vue
+++ src/components/views/Products.vue
@@ -1,8 +1,29 @@
 <script setup>
-
+  
+  // The imports
   import { products } from "../../data/product.json";
+  import { computed, ref } from "vue";
+  import Card from './Card.vue';
+  
   import add from "../../assets/add.svg";
-  import Card from './Card.vue'
+  import bag from "../../assets/bag.svg";
+  import BasketCard from './BasketCard.vue'
+
+  // The const that contain the data importing to the json file
+  const pro = ref(products);
+  // The variable to the value of the input
+  const inp = ref("")
+
+  // The variable that conatain the filtered list
+  const filteredProducts = computed(() => {
+    const input = inp.value;
+    return pro.value.filter(product => product.name.toLowerCase().includes(input.toLowerCase()));
+  })
+
+  // The function to change the value of the input
+  function onInput(e){
+    inp.value = e.target.value
+  }
 
 </script>
 
@@ -13,18 +34,37 @@
 
       <div class="search">
         <label for="search">Search item</label>
-        <input class="search_input" type="text" name="search" placeholder="Apple Watch, Samsung S21, Macbook Pro...">
+        <input @input="onInput" class="search_input" type="text" name="search" placeholder="Apple Watch, Samsung S21, Macbook Pro...">
       </div>
       <div class="products">
 
-        <Card v-for="product in products" :key="product.id" :products="product" :add="add" />
+        <Card v-for="product in filteredProducts" :key="product.id" :products="product" :add="add" />
   
       </div>
 
     </div>
 
-    <div>
-      <p>basket</p>
+    <div class="basket_div">
+      <div class="basket">
+        <p>Bag</p>
+
+        <div class="basket_product">
+
+          <BasketCard :pro="pro" />
+          <BasketCard :pro="pro" />
+          <BasketCard :pro="pro" />
+          <BasketCard :pro="pro" />
+
+
+        </div>
+
+        <a href="" class="button">
+          <div class="basket_add">
+            <img :src="bag" alt="bag">
+            <p>View Bag</p>
+          </div>
+        </a>
+      </div>
     </div>
 
   </div>
@@ -32,13 +72,61 @@
 
 <style scoped>
 
+  .button{
+    text-decoration: none;
+    color: white;
+  }
+  .basket_product{
+    display: flex;
+    gap: 10px;
+    padding: 10px;
+    height: 200px;
+    overflow: auto;
+    scrollbar-width: none;
+    flex-wrap: wrap;
+    border: 2px solid white;
+    border-radius: 10px;
+  }
+  .basket_add{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    height: 35px;
+    padding: 0px 10px 0px 10px;
+    background-color: black;
+    border-radius: 10px;
+  }
+  .basket{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 70%;
+    height: 45%;
+    border-radius: 10px;
+    background-color: gray;
+    padding: 0px 10px 0px 10px;
+  }
+  .basket_div{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    border-left: 4px solid white;
+    border-right: 4px solid white;
+    padding-left: 20px;
+    padding-top: 10%;
+    border-radius: 20px;
+  }
   .search{
     display: flex;
     flex-direction: column;
   }
   .search_input{
     padding-left: 20px;
-    height: 30px;
+    height: 40px;
     width: 350px;
     border-radius: 5px;
     outline: none;
